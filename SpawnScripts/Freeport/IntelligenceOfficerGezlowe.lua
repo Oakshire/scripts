b20b6139d7f2d11f668e756a5a8de36df8d59aaa
Cynnar <jacob0305@gmail.com>
2020-08-09 10:40:10 +0000

    Automatic Content Update

--- SpawnScripts/Freeport/IntelligenceOfficerGezlowe.lua
+++ SpawnScripts/Freeport/IntelligenceOfficerGezlowe.lua
@@ -6,9 +6,24 @@
                    : 
 --]]
 
+local QUEST = 573
+
 function spawn(NPC)
+ProvidesQuest(NPC, QUEST)
+SetPlayerProximityFunction(NPC, 10, "InRange", "LeaveRange", Spawn)
+end
+
+
+function InRange(NPC, Spawn)
+if HasQuest(Spawn, QUEST)
+PlayFlavor(NPC, "", "You there!  If you don't wish to be found in violation of treason, you'd better have a word with me.", "attention", 1689589577, 4560189, Spawn)
+end
+   end
+
+function LeaveRange(NPC, Spawn)
 
 end
+
 
 function hailed(NPC, Spawn)
     FaceTarget(NPC, Spawn)
