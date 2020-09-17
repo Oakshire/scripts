1dd1bc1655baa468ea66bbc4f7ff2d271eaa2c83
Cynnar <jacob0305@gmail.com>
2020-09-17 10:40:10 +0000

    Nightly Update

--- Spells/Mage/Sorcerer/Warlock/ShroudofBertoxxulous.lua
+++ Spells/Mage/Sorcerer/Warlock/ShroudofBertoxxulous.lua
@@ -1,11 +1,11 @@
 --[[
     Script Name    : Spells/Mage/Sorcerer/Warlock/ShroudofBertoxxulous.lua
     Script Author  : neatz09
-    Script Date    : 2020.02.29 04:02:36
+    Script Date    : 2020.09.17 02:09:14
     Script Purpose : 
                    : 
 --]]
 
 -- Info from spell_display_effects (remove from script when done)
 -- When damaged this spell has a 25% chance to cast Nihilistics on target's attacker.  
---     Inflicts 13 - 16 poison damage on target encounter
+--     Inflicts 39 - 47 poison damage on target encounter
