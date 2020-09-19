f471ea4678bcbab2f4417b7dc8cac4c72c983d4e
Cynnar <jacob0305@gmail.com>
2020-09-19 10:40:09 +0000

    Nightly Update

--- Spells/Mage/Sorcerer/FlamesofVelious.lua
+++ Spells/Mage/Sorcerer/FlamesofVelious.lua
@@ -1,7 +1,7 @@
 --[[
     Script Name    : Spells/Mage/Sorcerer/FlamesofVelious.lua
     Script Author  : neatz09
-    Script Date    : 2020.05.14 09:05:26
+    Script Date    : 2020.09.19 01:09:17
     Script Purpose : 
                    : 
 --]]
@@ -10,3 +10,15 @@
 -- Inflicts 190 heat damage on target
 -- Inflicts 190 cold damage on target
 -- Slows target by 33.3%
+
+function cast(Caster, Target, DmgType, MinVal, MaxVal, DmgType2, MinVal2, MaxVal2, SlowAmt)
+local Slow = 100 - SlowAmt
+	SpellDamage(Target, DmgType, MinVal, MaxVal)
+	SpellDamage(Target, DmgType2, MinVal2, MaxVal2)
+	SetSpeedMultiplier(Target, Slow)
+end
+
+
+function remove(Caster, Target)
+SetSpeedMultiplier(Target, 1)
+end
--- Spells/Scout/Bard/Dirge/GarsinsFuneralMarch.lua
+++ Spells/Scout/Bard/Dirge/GarsinsFuneralMarch.lua
@@ -6,8 +6,29 @@
                    : 
 --]]
 
--- Info from spell_display_effects (remove from script when done)
 -- Makes target afraid
 -- 30% chance to dispel when target takes damage
 -- Does not affect Epic targets
 -- Resistibility increases against targets higher than level 29.
+local Spell = GetSpellID()
+
+function cast(Caster, Target, Chance)
+    Say(Caster, "Resist level 29 or < not implemented.")
+	Say(Caster, "Need a check for ifhascontroleffect")
+	if not IsEpic(Target) then
+		AddControlEffect(Target, 6)
+			end
+	AddProc(Target, 15, Chance)
+end
+
+function proc(Caster, Target, Type, Chance)
+	if Type == 15 and HasSpellEffect(Target, Spell) then
+		RemoveControlEffect(Target, 6)
+			end
+end
+
+function remove(Caster, Target)
+	RemoveControlEffect(Target, 6)
+	RemoveProc(Target)
+	
+end
