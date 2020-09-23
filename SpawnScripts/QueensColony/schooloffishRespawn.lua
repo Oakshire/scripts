34eb66402fa74212865db256fa0b8635d83c3728
Cynnar <jacob0305@gmail.com>
2020-09-23 10:40:10 +0000

    Nightly Update

--- SpawnScripts/QueensColony/schooloffishRespawn.lua
+++ SpawnScripts/QueensColony/schooloffishRespawn.lua
@@ -2,19 +2,15 @@
     Script Name    : SpawnScripts/QueensColony/schooloffishRespawn.lua
     Script Author  : Rylec
     Script Date    : 2020.09.14 12:09:03
-    Script Purpose : 
+    Script Purpose : Random respawn time for School of Fish
                    : 
 --]]
 
 function spawn(NPC)
---        math.randomseed(os.time())
---        local respawnTime = math.random (5,20)
+        math.randomseed(os.time())
+        local respawnTime = math.random (60,120)
 
-        SpawnSet(NPC, "respawn", "30") 
-
---        local expireTime = math.random (5,20)
-
-        SpawnSet(NPC, "expire", "10") 
+        SpawnSet(NPC, "respawn", respawnTime) 
 end
 
 function respawn(NPC)
