4fc5026874632362e760b484542cac6a0e48d363
Cynnar <jacob0305@gmail.com>
2020-10-28 10:40:11 +0000

    Nightly Update

--- Spells/Fighter/Warrior/Guardian/IronWill.lua
+++ Spells/Fighter/Warrior/Guardian/IronWill.lua
@@ -1,12 +1,12 @@
 --[[
     Script Name    : Spells/Fighter/Warrior/Guardian/IronWill.lua
     Script Author  : neatz09
-    Script Date    : 2019.11.14 08:11:29
+    Script Date    : 2020.10.28 03:10:39
     Script Purpose : 
                    : 
 --]]
 
 -- Info from spell_display_effects (remove from script when done)
--- Increases STA of caster by 29.6
--- Reduces slow effects on caster by 10.3%
--- Increases Mitigation of caster vs elemental, noxious and arcane damage by 777
+-- Increases STA of caster by 37.1
+-- Reduces slow effects on caster by 13.5%
+-- Increases Mitigation of caster vs elemental, noxious and arcane damage by 972
