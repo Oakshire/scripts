bad2a8dd003432506096a2785365fe90c0826d52
Cynnar <jacob0305@gmail.com>
2020-11-20 10:40:12 +0000

    Nightly Update

---
+++ SpawnScripts/QueensColony/ataintedSapswillinvader431100.lua
@@ -0,0 +1,37 @@
+--[[
+        Script Name     :       SpawnScripts/QueensColony/ataintedSapswillinvader431100.lua
+	Script Purpose	:	Waypoint Path for ataintedSapswillinvader431100.lua
+	Script Author	:	Rylec
+	Script Date	:	11-19-2020 05:12:27 
+	Script Notes	:	Locations collected from Live
+--]]
+
+function spawn(NPC)
+	waypoints(NPC)
+end
+
+function hailed(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+end
+
+function respawn(NPC)
+        spawn(NPC)
+end
+
+function waypoints(NPC)
+	MoveToLocation(NPC, 174.34, -3.11, 155.46, 2)
+	MoveToLocation(NPC, 84.66, -4.54, 171.98, 2)
+
+
+	MoveToLocation(NPC, 178.12, -3.2, 142.29, 2)
+	MoveToLocation(NPC, 171.5, -3.34, 145.71, 2)
+	MoveToLocation(NPC, 75.82, -4.48, 180.13, 2)
+	MoveToLocation(NPC, 68.99, -4.49, 177.68, 2, "waypointsLoop")
+end
+
+function waypointsLoop(NPC)
+	MovementLoopAddLocation(NPC, 60.48, -4, 189.65, 2, 0)
+	MovementLoopAddLocation(NPC, 72.34, -4.3, 169.86, 2, 0)
+	MovementLoopAddLocation(NPC, 67.34, -4.28, 154.56, 2, 0)
+	MovementLoopAddLocation(NPC, 71.9, -4.3, 170.88, 2, 0)
+end
