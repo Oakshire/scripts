006ad98bb18df9c12173b68c66e144ae72888f06
Cynnar <jacob0305@gmail.com>
2021-01-01 10:40:20 +0000

    Nightly Update

--- SpawnScripts/QueensColony/TimboAlefirst.lua
+++ SpawnScripts/QueensColony/TimboAlefirst.lua
@@ -22,23 +22,17 @@ function hailed(NPC, Spawn)
     voice = math.random (1,3)
     str = tostring(choice)
 
-	if voice == 1 then
-            PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_1_1026.mp3", "", "", 0, 0, Spawn)
-	elseif voice == 2 then
-	    PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_2_1026.mp3", "", "", 0, 0, Spawn)
-	else
-	    PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_3_1026.mp3", "", "", 0, 0, Spawn)
-        end
+    PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_"..choice.."_1026.mp3", "", "", 0, 0, Spawn)
 
-        text = math.random(1,3)
+    text = math.random(1,3)
 
-        if text == 1 then
-            Say(NPC, "Wow, Meri is a great dancer!")
-        elseif text == 2 then
-            Say(NPC, "We need a band!")
-        else
-            Say(NPC, "I hope im not embarrassing myself.")	
-        end
+    if text == 1 then
+        Say(NPC, "Wow, Meri is a great dancer!")
+    elseif text == 2 then
+        Say(NPC, "We need a band!")
+    else
+        Say(NPC, "I hope I'm not embarrassing myself.")	
+    end
 end
 
 function EmoteLoop(NPC)
@@ -49,7 +43,7 @@ function EmoteLoop(NPC)
     if choice == 1 then
 -- flirt
 	PlayAnimation(NPC, 11557)
-	AddTimer(NPC, 11000, "Idle")
+	AddTimer(NPC, 9600, "Idle")
     elseif choice == 2 then
 -- orate
 	PlayAnimation(NPC, 11909)
@@ -57,11 +51,11 @@ function EmoteLoop(NPC)
     elseif choice == 3 then
 -- flourish
 	PlayAnimation(NPC, 298)
-	AddTimer(NPC, 9000, "Idle")
+	AddTimer(NPC, 8000, "Idle")
     else
 -- heelclick
 	PlayAnimation(NPC, 11681)
-	AddTimer(NPC, 3500, "Idle")
+	AddTimer(NPC, 2500, "Idle")
     end
 end
 
