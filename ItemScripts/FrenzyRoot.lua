4bbbf8bb02405893aa7b5bcc44ac023ce7bc6665
Cynnar <jacob0305@gmail.com>
2021-03-25 10:40:11 +0000

    Nightly Update

--- ItemScripts/FrenzyRoot.lua
+++ ItemScripts/FrenzyRoot.lua
@@ -8,14 +8,15 @@
 
 
            
-function used(Item, Spawn)
-         local zone = GetZone(Spawn)
-	     local X = GetX(Spawn)
-	     local Y = GetY(Spawn)
-	     local Z = GetZ(Spawn)
-        local FrenzyRoot = SpawnMob(zone, 331123, false, X, Y, Z, 50)
-        if FrenzyRoot ~= nil then
-        AddSpawnAccess(FrenzyRoot, Spawn)
-        RemoveItem(Spawn, 46427)           
-        end
-           end
+function used(Item, Player)
+    SendMessage(Player, "You have used an item", "yellow")
+    local zone = GetZone(Player)
+	local X = GetX(Player)
+	local Y = GetY(Player)
+	local Z = GetZ(Player)
+    local FrenzyRoot = SpawnMob(zone, 331123, false, X, Y, Z, 50)
+    if FrenzyRoot ~= nil then
+        AddSpawnAccess(FrenzyRoot, Player)
+        RemoveItem(Player, 46427)           
+    end
+end
