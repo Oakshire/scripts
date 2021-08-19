8f77a7bfce9574f58199320c96b633bf51477480
Cynnar <jacob0305@gmail.com>
2021-08-19 10:40:11 +0000

    Nightly Update

--- SpawnScripts/qeynos_combined02/PreservationistReynolds.lua
+++ SpawnScripts/qeynos_combined02/PreservationistReynolds.lua
@@ -12,9 +12,12 @@ end
 
 function hailed(NPC, Spawn)
 	FaceTarget(NPC, Spawn)
-    local voice = MakeRandomInt(1,3)
 
-    PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_"..voice.."_1047.mp3", "...", "hello", 0, 0, Spawn)
+    PlayFlavor(NPC, "voiceover/english/preservationist_reynolds/qey_elddar/100_soc_human_tunarian_officer_reynolds_no_2a5aeb44.mp3", "I haven't heard anything from the last scout party.  They're a tough bunch. I hope they're all right.", "", 1752705355, 1563128162, Spawn)
+
+--    local voice = MakeRandomInt(1,3)
+
+--    PlayFlavor(NPC, "voiceover/english/voice_emotes/greetings/greetings_"..voice.."_1047.mp3", "...", "hello", 0, 0, Spawn) -- To be used at certain times when their is no voice over option (based on player/guild level?)
 end
 
 function respawn(NPC)
