04e90acd466eef5064cc0b5726add55ab2518cdd
Cynnar <jacob0305@gmail.com>
2021-10-20 10:40:17 +0000

    Nightly Update

--- SpawnScripts/Baubbleshire/MirfGuinders.lua
+++ SpawnScripts/Baubbleshire/MirfGuinders.lua
@@ -6,6 +6,8 @@
 	Script Notes	: 
 --]]
 
+local InSearchoftheSepulcherofJahnda = 556
+
 function spawn(NPC)
 end
 
@@ -15,10 +17,55 @@ end
 
 function hailed(NPC, Spawn)
 	FaceTarget(NPC, Spawn)
-	local choice = math.random(1, 2)
+	if GetQuestStep(Spawn, InSearchoftheSepulcherofJahnda) == 1 then
+	local conversation = CreateConversation()
+	PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders000.mp3", "", "", 35546, 2556985953, Spawn)
+	AddConversationOption(conversation, "Do you know anything about a Sepulcher? ", "Option1")
+	AddConversationOption(conversation, "I don't want to hear your lunacy. Farewell.")
+	StartConversation(conversation, NPC, Spawn, "Search within your soul! Reclaim your faith! Bristlebane lives within us. Do not abandon his mirth! We are not forsaken!")
+    elseif GetQuestStep(Spawn, InSearchoftheSepulcherofJahnda) == 3 then
+    local conversation = CreateConversation()
+    PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders000.mp3", "", "", 35546, 2556985953, Spawn)
+    AddConversationOption(conversation, "I have brought your crystals. Where are my directions?", "Option3")
+   	AddConversationOption(conversation, "I don't want to hear your lunacy. Farewell.")
+	StartConversation(conversation, NPC, Spawn, "Search within your soul! Reclaim your faith! Bristlebane lives within us. Do not abandon his mirth! We are not forsaken!")
+	else
+	local choice = MakeRandomInt(1, 2)
 	if choice == 1 then
-		PlayFlavor(NPC, "", "Search within your soul! Reclaim your faith! Bristlebane lives within us. Do not abandon his mirth! We are not forsaken!", "", 1689589577, 4560189, Spawn)
+		PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders000.mp3", "Search within your soul! Reclaim your faith! Bristlebane lives within us. Do not abandon his mirth! We are not forsaken!", "", 35546, 2556985953, Spawn)
 	elseif choice == 2 then
 		PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/100_mirf_guinders_multhail1_59e52ebd.mp3", "Good adventurer, have faith and be well!  Perhaps one day you too may serve Bristlebane!", "", 300423478, 2139393363, Spawn)
 	end
+ end
 end
+
+
+function Option1(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+	local conversation = CreateConversation()
+    PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders006.mp3", "", "", 303473158, 262617160, Spawn)
+	AddConversationOption(conversation, "Where can I find this Sepulcher?", "Option2")
+	AddConversationOption(conversation, "I am not interested in the Sepulcher. Farewell. ")
+	StartConversation(conversation, NPC, Spawn, "The Sepulcher of Jahnda? I know of it. I happened to discover its location when I was in a part of the catacombs the locals call Vermin's Snye.  ")
+end
+
+function Option2(NPC, Spawn)
+    SetStepComplete(Spawn, InSearchoftheSepulcherofJahnda, 1)
+	FaceTarget(NPC, Spawn)
+	local conversation = CreateConversation()
+    PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders007.mp3", "", "", 1102575551, 1768318615, Spawn)
+	AddConversationOption(conversation, "I shall return with your donation.  ")
+	StartConversation(conversation, NPC, Spawn, "When the temple of Mirf Guinders has a donation of jade golem crystals then I will offer you directions to the Sepulcher of Jahnda.")
+end
+
+function Option2(NPC, Spawn)
+    SetStepComplete(Spawn, InSearchoftheSepulcherofJahnda, 3)
+	FaceTarget(NPC, Spawn)
+	local conversation = CreateConversation()
+     PlayFlavor(NPC, "voiceover/english/mirf_guinders/qey_village06/mirfguinders008.mp3", "", "", 2667033451, 2560833588, Spawn)
+	AddConversationOption(conversation, "Thank you for your help. Farewell.  ")
+	StartConversation(conversation, NPC, Spawn, "Ah, yes, the key. Unfortunately the key was swiped from my pack by a wretched being called the Rat Queen, wrangler of rodents. Find her and you find your key. Then you can follow my notes to the Sepulcher of Jahnda.")
+end
+
+
+
