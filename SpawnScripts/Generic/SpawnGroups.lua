f3fdec2d7105d35b7283e35c5ddba069a3248ca0
Cynnar <jacob0305@gmail.com>
2022-01-16 10:40:11 +0000

    Nightly Update

--- SpawnScripts/Generic/SpawnGroups.lua
+++ SpawnScripts/Generic/SpawnGroups.lua
@@ -1,10 +1,7 @@
 --[[
-	Script Name	:	SpawnScripts/ThievesWay/ASewerSlime.lua
-	Script Purpose	:	Waypoint Path for ASewerSlime.lua
-	Script Author	:	Rylec
-	Script Date	:	09-11-2019 08:43:31 
-	Script Notes	:	Don't delete this spawn - its in a group
+	Script Name		:	SpawnScripts/Generic/SpawnGroups.lua
+	Script Purpose	:	Waypoint Path
+	Script Author	:Rylec
+	Script Date		:09-11-2019
+	Script Notes	:Don't delete - part of a group spwan
 --]]
-
-
-
