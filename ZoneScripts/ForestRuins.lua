eb53df68b409ae941eb82b7915101de2790f8bc0
Cynnar <jacob0305@gmail.com>
2022-03-03 10:40:08 +0000

    Nightly Update

--- ZoneScripts/ForestRuins.lua
+++ ZoneScripts/ForestRuins.lua
@@ -6,15 +6,15 @@
 	Script Notes	:	
 --]]
 
+--local BeachTnarl =  GetSpawnGroupID(zone, 1049741)
+
 function init_zone_script(zone)
 end
 
 function player_entry(zone, player)
     
-    
-   -- Say(player, "Wow! Forest Ruins is so pretty (v3)")
 	if  HasQuest(player , 522) then
-        Say(player, "You have quest 522")
+--        Say(player, "You have quest 522")
         newspawn1 = SpawnMob(zone, 1960182, true, 1161.29, -21.6585, -741.387, 0)
         newspawn2 = SpawnMob(zone, 1960182, true, 1165.24, -21.7587, -738.934, 0)
         newspawn3 = SpawnMob(zone, 1960182, true, 1163.75, -21.6419, -744.377, 0)
@@ -46,14 +46,14 @@ end
 
 function leave_location(zone, spawn, grid)
 end
-
+--[[
 function callout(zone,Spawn)
 	local players = GetPlayersInZone(zone)
 	for index, player in ipairs(players) do
 		SendPopUpMessage(player, "Grinding stone can be heard as something ancient stirs in the ruins.", 255, 0, 0)
 		SendMessage(player, "Grinding stone can be heard as something ancient stirs in the ruins.")
 	end
-end
+end]]--
 
 function dawn(zone)
 	local players = GetPlayersInZone(zone)
@@ -61,6 +61,9 @@ function dawn(zone)
 		SendPopUpMessage(player, "As dawn breaks, the undead retreat to the shadows as the Blackfurl forces flood the castle.", 255, 0, 0)
 		SendMessage(player, "As dawn breaks, the undead retreat to the shadows as the Blackfurl forces flood the castle.")
 	end
+    	if GetSpawnLocationID (zone, 133772890) ~= nil then
+	    Despawn (NPC, 133772890)
+end
 end
 
 function dusk(zone)
@@ -69,4 +72,13 @@ function dusk(zone)
 		SendPopUpMessage(player, "As dusk falls, the Blackfurl forces retreat to the beach as the undead crawl from the shadows.", 255, 0, 0)
 		SendMessage(player, "As dusk falls, the Blackfurl forces retreat to the beach as the undead crawl from the shadows.")
 	end
+--[[	if GetSpawnLocationID(zone, 133772890) == nil then
+--        SpawnGroupByID(zone, 1049741)	
+--          GetSpawnGroupID(zone, 1049741) 
+	    SpawnByLocationID(zone, 133772890) 
+	  for index, player in ipairs(players) do
+		SendPopUpMessage(player, "Dig Supervisor T'narl is preparing to raid the castle from the beachfront.", 255, 0, 0)
+		SendMessage(player, "Dig Supervisor T'narl is preparing to raid the castle from the beachfront.")
+			end
+	end]]--
 end
