245b4f91d99da569ade9619ef23f2eeb2bec04f1
Cynnar <jacob0305@gmail.com>
2022-04-12 10:40:25 +0000

    Nightly Update

--- SpawnScripts/Stormhold/adefiledsentry.lua
+++ SpawnScripts/Stormhold/adefiledsentry.lua
@@ -6,10 +6,28 @@
                    : 
 --]]
 
+local level1 = 16
+local difficulty1 = 7
+local hp1 = 2176
+local power1 = 789
+local level2 = 15
+local difficulty2 = 7
+local hp2 = 3484
+local power2 = 1439
 
 function spawn(NPC)
-
-end
+    local Level = GetLevel(NPC)
+    if Level == level1 then
+    SpawnSet(NPC, "difficulty1", 6)
+    SpawnSet(NPC, "hp1", 2176)
+    SpawnSet(NPC, "power1", 789)
+    elseif Level == level2
+        then
+    SpawnSet(NPC, "difficulty2", 7)
+    SpawnSet(NPC, "hp2", 3484)
+    SpawnSet(NPC, "power2", 1439)
+    end
+    end
 
 function hailed(NPC, Spawn)
 	FaceTarget(NPC, Spawn)
@@ -21,16 +39,3 @@ end
 
 
 
-
-function prespawn(NPC)
-local Level = GetLevel(NPC)
-if Level == 16 then
-SpawnSet(NPC, "difficulty", 6)
-SpawnSet(NPC, "hp", 2176)
-SpawnSet(NPC, "power", 789)
-elseif Level == 15 then
-SpawnSet(NPC, "difficulty", 7)
-SpawnSet(NPC, "hp", 3484)
-SpawnSet(NPC, "power", 1439)
-end
-   end
