f1b28b7c311574919e98af0d3241fc483f0e4dcc
Cynnar <jacob0305@gmail.com>
2022-11-09 10:41:31 +0000

    Updated 09/11/2022

--- SpawnScripts/Qeynos/AugurPrichardFizzburn.lua
+++ SpawnScripts/Qeynos/AugurPrichardFizzburn.lua
@@ -5,29 +5,3 @@
 	Script Date	: 2020.04.12
 	Script Notes	: Auto-Generated Conversation from PacketParser Data
 --]]
-dofile("SpawnScripts/Generic/GenericGuardVoiceOvers.lua")
-dofile("SpawnScripts/Generic/ExpelNonCitizen.lua")
-
-
-function spawn(NPC)
-	SetPlayerProximityFunction(NPC, 10, "InRange", "LeaveRange")
-end
-
-function respawn(NPC)
-	spawn(NPC)
-end
-
-function InRange(NPC, Spawn)
-    NonCitizen(NPC,Spawn)    
-end
-
-function LeaveRange(NPC, Spawn)
-end
-
-function hailed(NPC, Spawn)
-	FaceTarget(NPC, Spawn)
-	GenericGuardHail(NPC, Spawn)
-
-
-end
-
---
+++ ZoneScripts/DownBelow_Classic.lua
@@ -0,0 +1,16 @@
+--[[
+    Script Name    : ZoneScripts/DownBelow_Classic.lua
+    Script Author  : LordPazuzu
+    Script Date    : 2022.11.09 03:11:52
+    Script Purpose : 
+                   : 
+--]]
+
+function init_zone_script(Zone)
+
+end
+
+function player_entry(Zone, Player)
+SendPopUpMessage(Player, "The Down Below", 230, 230, 230)   
+end
+
