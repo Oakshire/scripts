fef12377b8a21c230bf5f8ae41f35ae8a945788e
Cynnar <jacob0305@gmail.com>
2023-06-07 10:41:55 +0000

    Updated 07/06/2023

---
+++ ZoneScripts/TheSunkenCity_Classic.lua
@@ -0,0 +1,16 @@
+--[[
+    Script Name    : ZoneScripts/TheSunkenCity_Classic.lua
+    Script Author  : LordPazuzu
+    Script Date    : 2023.06.06 05:06:29
+    Script Purpose : 
+                   : 
+--]]
+
+function init_zone_script(Zone)
+
+end
+
+function player_entry(Zone, Player)
+
+end
+
